d582a1b8ef80946a71d782f7e4e68afcdfc3c601
Ritesh <riteshroyal834047@gmail.com>
2026-09-12 18:39:36 +0530

    refactor: remove web frontend to finalize standalone Terminal Application interface

--- frontend/src/components/WeatherCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import WeatherWidget from './WeatherWidget';
-
-const WeatherCard = ({ weather }) => {
-  return <WeatherWidget weather={weather} />;
-};
-
-export default WeatherCard;
--- frontend/src/pages/Profile.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Settings from './Settings';
-
-const Profile = () => {
-  return <Settings />;
-};
-
-export default Profile;
